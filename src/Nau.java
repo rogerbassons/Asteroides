dfc8543d76f48d27eee438eb7cada51dafdf2a7e
Bassons <rogerbassons@gmail.com>
2015-04-10 08:49:52 +0200

    Afegit metode per fer proves i una clase que ho prova

--- src/Nau.java
+++ src/Nau.java
@@ -10,7 +10,7 @@ public class Nau {
 	
 	//Pre: l > 0 i a > 0
 	//Post: la Nau té una llargada l i una amplada màxima a. La posició del centre de la Nau és (x,y) i la seva velocitat és zero.
-	Nau(int l, int a, int x, int y) {
+	Nau(int l, int a) { //, int x, int y) {
 		triangle_ = new Path2D.Float();
 		triangle_.moveTo(a/2,0);
 		triangle_.lineTo(0,l);
@@ -60,6 +60,26 @@ public class Nau {
 		}
 		centrex = (puntsT[0]+puntsT[2]+puntsT[4])/3;
 		centrey = (puntsT[1]+puntsT[3]+puntsT[5])/3;
+	}
+
+	/* TEST */
+	public float [] obtenirPuntsTriangle() {
+		float [] puntsT = new float[6]; 
+		float [] coordenades = new float[6];
+	
+		PathIterator pi = triangle_.getPathIterator(null,0);
+		pi.currentSegment(coordenades);
+	
+		puntsT[0] = coordenades[0];
+		puntsT[1] = coordenades[1];
+		int i = 2;
+		while (!pi.isDone()) {
+			pi.currentSegment(coordenades);
+			puntsT[i] = coordenades[2];
+			puntsT[i+1] = coordenades[3];
+			pi.next();
+		}
+		return puntsT;
 	}
 }
 	
---
+++ test/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class Main {
+	public static void main(String[] args) {
+		Nau n = new Nau(8,4);
+		float [] punts = n.obtenirPuntsTriangle();
+
+		for (float i : punts) {
+			System.out.println(i);
+		}
+	}
+
+}
