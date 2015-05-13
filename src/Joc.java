9abebbe1931c30cf64dc9b8b733d7180080fcf8d
mfarreras <miquel2222@gmail.com>
2015-05-13 10:50:58 +0200

    Afegits comentaris i creat mètodes per moure les coses, per fer-ho llegible

--- src/Joc.java
+++ src/Joc.java
@@ -14,12 +14,56 @@ import java.util.GregorianCalendar;
 import java.util.Calendar;
 import java.awt.Shape;
 
-//Mòdul que gestiona la lògica, la física i la interfície del joc
+//Mòdul que gestiona la lògica, la física i la interfície del joc.
 //El Joc conté quatre tipus d'elements:
-//	- Nau: Nau pròpia controlada per l'usuari
-//	- NauEnemiga: Nau controlada per la màquina (AI)
-//	- Meteorits: Objectes controlats per la màquina
-//	- RaigLasers: raig disparat per Nau i NauEnemiga, que destrueix Meteorits i Naus
+//	- Nau: Nau pròpia controlada per l'usuari.
+//	- NauEnemiga: Nau controlada per la màquina (AI).
+//	- Meteorits: Objectes controlats per la màquina, que van a la deriva.
+//	- RaigLasers: raig disparat per Nau i NauEnemiga, que destrueix Meteorits i Naus.
+//
+//Funcionament general del Joc:
+//Nau:
+//	- Rota sobre si mateixa. Té un coet propulsor que l'impulsa endavant.
+//	- Quan abandoni l'espai visible per l'usuari, per algun dels costats de la finestra, apareixerà pel costat invers, conservant el moviment que portava.
+//	- Té forma de triangle isòsceles.
+//	- Si s'acaben les vides de la Nau, la partida s'ha acabat.
+//	- Un cop s'ha començat a moure en una direcció, es continua movent en aquesta direcció durant un temps determinat mentre l'usuari no intervingui, simulant 
+//	  la ingravidesa de l'espai, i al mateix temps, facilitant el control de la nau.
+//
+//NauEnemiga:
+//	- Hi ha una NauEnemiga que intenta, per qualsevol mitjà (disparant i col·lisionant), destruir la nau de l'usuari. 
+//	- Té el mateix comportament que la Nau espacial controlada per l'usuari.
+//	- Excepte: 
+//		- L'objectiu d'aquesta nau és destruir la nau de l'usuari.
+//		- Per a destruir la nau de l'usuari dispara rajos làser i la persegueix.
+//
+//	- La Nau i la NauEnemiga poden disparar RajosLaser.
+//
+//RaigLaser:
+//	- Els RaigLaser poden col·lisionar amb Meteorits o amb les Naus.
+//
+//Meteorit:
+//	- És un objecte que es mou amb una velocitat i direcció pseudoaleatòries (entre un rang determinat)
+//	- Té una forma d'un polígon irregular, pseudoaleatòria
+//	- Els Meteorits no col·lisionen entre si, s'atravessen.
+//	- Quan un Meteorit gran col·lisiona amb un RaigLaser o una Nau, es divideix en dos Meteorits petits.
+//	- Si xoca contra una nau, destrueix la nau amb la qual ha xocat.
+//	- Quan un Meteorit petit col·lisiona amb un RaigLaser o una Nau, aquest desapareix.
+//
+//Descripció general:
+//Inicialment, hi ha meteorits grans dispersats per tot l'espai i la nau enemiga a prop d'algun extrem de l'espai. La nau controlada per l'usuari està al centre
+//i cap meteorit està a sobre seu. Hi ha un límit definit de meteorits que poden estar dins l'espai del joc. Quan es comença, hi ha un número determinat de meteorits grans, 
+//que es va augmentant fins al límit. A partir de llavors, cada vegada que es destrueixi un meteorit n'apareix un de nou. 
+//
+//Només hi ha una nau enemiga a l'espai. Cada vegada que sigui destruïda, n'apareix una de nova. 
+//
+//El joc té un sistema de puntuació i de vides. L'usuari sempre comença amb 3 vides i 0 punts. A mesura que va destruint meteorits i naus enemigues, augmenta la seva puntuació, 
+//segons aquestes ponderacions: meteorit gran 50 punts, meteorit petit 20 punts, nau enemiga 100 punts. 
+//Els controls són els següents:
+//	- W: impulsar cap endavant
+//	- A: rotar cap a l'esquerra
+//	- D: rotar cap a la dreta
+//	- Espai: disparar rajos làser
 
 public class Joc {
 	
@@ -71,10 +115,10 @@ public class Joc {
 		d_.afegirKeyListener(new MyKeyListener());
 		d_.afegir(n_);
 		d_.afegir(ne_);
-
+		
 		tempsRaigEnemiga_ = new GregorianCalendar();
 		tempsRaigJugador_ = new GregorianCalendar();
-
+		
 		nVides_ = 3;
 		puntuacio_ = 0; 
 	}
@@ -97,11 +141,11 @@ public class Joc {
 		}
 		
 		d_.tancarFinestra();
-	
+		
 	}
 	
 	//Pre: --
-	//Post: afegeix Meteorits al DibuixadorAsteroides i al Joc, fins a un màxim de 10
+	//Post: afegeix Meteorits al DibuixadorAsteroides i al Joc, fins a un màxim de 10, evitant que estiguin sobre la Nau
 	private void generarMeteoritsInicials() throws Exception {
 		while (meteorits_.size() < 10) {
 			Random rand = new Random();
@@ -133,23 +177,20 @@ public class Joc {
 	//Pre: --
 	//Post: actualitza l'estat del joc, generant meteorits, movent els elements i tractant les col·lisions
 	private void actualitzar() throws Exception {
+		generarMeteorits(); //Generem Meteorits
 		
-		generarMeteorits();
+		moureNaus(); //Movem tots els objectes
 		
-		if (rotarDreta_) {
-			n_.rotarDreta();
-		} else if (rotarEsquerra_) {
-			n_.rotarEsquerra();
-		} else {
-			n_.pararRotacio();
-		}
-
-		if (accelerar_) {
-			n_.propulsarEndavant();
-		}
-
-		n_.moure(amplada_,altura_);
-
+		dispararNaus(); //Disparen les Naus (si cal)
+		
+		moureMeteoritsIRajosLaser(); //Movem els RajosLaser i els Meteorits
+		
+		tractarColisions(); //Tractem les possibles col·lisions
+	}
+	
+	//Pre: --
+	//Post: si la Nau ha de disparar, dispara i afegeix el RaigLaser a la llista de RaigLaser del Joc, fent el mateix amb la NauEnemiga
+	private void dispararNaus() throws Exception {
 		if (disparar_) {
 			Calendar tempsActual = new GregorianCalendar();
 			if (tempsActual.getTimeInMillis() - tempsRaigJugador_.getTimeInMillis() > 441) {
@@ -173,12 +214,34 @@ public class Joc {
 				rajosLaser_.add(ra);
 			}
 		}
+	}
+	
+	//Pre: --
+	//Post: mou les Naus del Joc
+	private void moureNaus() {
 		
-		ne_.moure(amplada_,altura_);
+		//Tractem el moviment de la Nau
+		if (rotarDreta_)
+			n_.rotarDreta();
+		else if (rotarEsquerra_)
+			n_.rotarEsquerra();
+		else
+			n_.pararRotacio();
 		
+		if (accelerar_)
+			n_.propulsarEndavant();
+		
+		n_.moure(amplada_,altura_); //Movem la Nau
+		ne_.moure(amplada_,altura_); //Movem la NauEnemiga
+		
+	}
+	
+	//Pre: --
+	//Post: mou els Meteorits i els RajosLaser del Joc
+	private void moureMeteoritsIRajosLaser() throws Exception {
 		Iterator<RaigLaser> it = rajosLaser_.iterator();
-
-		while (it.hasNext()){
+		
+		while (it.hasNext()){ //Iterem per tots els RajosLaser per a moure'ls
 			RaigLaser r = it.next();
 			if (!r.gastat())
 				r.moure(amplada_, altura_);
@@ -188,12 +251,8 @@ public class Joc {
 			}
 		}
 		
-		Iterator<Meteorit> it2 = meteorits_.iterator();
-		while (it2.hasNext())
-			it2.next().moure(amplada_, altura_);
-		
-		tractarColisions();
-		
+		for (Meteorit m : meteorits_) //Iterem per tots els Meteorits per a moure'ls
+			m.moure(amplada_, altura_);
 	}
 	
 	//Pre: --
@@ -219,10 +278,10 @@ public class Joc {
 	//Post: si la nau està viva es centra la nau i se li resta una vida, altrament no fa res (de moment). Si té 0 vides, mor.
 	private void xocarNauJugador() throws Exception {
 		//if (n_.esViva()){
-			n_.centrar(amplada_, altura_);
-			nVides_--;
-			//if (nVides_ == 0) 
-				//n_.morir();
+		n_.centrar(amplada_, altura_);
+		nVides_--;
+		//if (nVides_ == 0) 
+		//n_.morir();
 		//}
 		//FALTA TRACTAR MORT
 	}
@@ -259,7 +318,7 @@ public class Joc {
 			RaigLaser r = it.next();
 			Area rl = new Area(r.obtenirShape());
 			boolean haXocat = tractarColisionsAmbMeteorits(rl);
-
+			
 			if (haXocat) {
 				it.remove();
 				d_.elimina(r);
@@ -307,7 +366,7 @@ public class Joc {
 		
 		boolean haXocatNau = tractarColisionsAmbMeteorits(n);
 		boolean haXocatNE = tractarColisionsAmbMeteorits(ne);
-
+		
 		if (haXocatNau) 
 			xocarNauJugador();
 		
@@ -357,41 +416,41 @@ public class Joc {
 	public class MyKeyListener implements KeyListener {
 		
 		public void keyTyped(KeyEvent e) {}
-
+		
 		public void keyPressed(KeyEvent e) {
 			switch(e.getKeyCode()) {
-			case KeyEvent.VK_SPACE:
-				disparar_ = true;
-				break;
-			case KeyEvent.VK_W:
-				accelerar_ = true;
-				break;
-			case KeyEvent.VK_D:
-				rotarDreta_ = true;
-				break;
-			case KeyEvent.VK_A:
-				rotarEsquerra_ = true;
-				break;
+				case KeyEvent.VK_SPACE:
+					disparar_ = true;
+					break;
+				case KeyEvent.VK_W:
+					accelerar_ = true;
+					break;
+				case KeyEvent.VK_D:
+					rotarDreta_ = true;
+					break;
+				case KeyEvent.VK_A:
+					rotarEsquerra_ = true;
+					break;
 			}
 		}
 		
 		public void keyReleased(KeyEvent e) {
 			switch(e.getKeyCode()) {
-			case KeyEvent.VK_SPACE:
-				disparar_ = false;
-				break;
-			case KeyEvent.VK_ESCAPE:
-				sortir_ = true;
-				break;
-			case KeyEvent.VK_W:
-				accelerar_ = false;
-				break;
-			case KeyEvent.VK_D:
-				rotarDreta_ = false;
-				break;
-			case KeyEvent.VK_A:
-				rotarEsquerra_ = false;
-				break;
+				case KeyEvent.VK_SPACE:
+					disparar_ = false;
+					break;
+				case KeyEvent.VK_ESCAPE:
+					sortir_ = true;
+					break;
+				case KeyEvent.VK_W:
+					accelerar_ = false;
+					break;
+				case KeyEvent.VK_D:
+					rotarDreta_ = false;
+					break;
+				case KeyEvent.VK_A:
+					rotarEsquerra_ = false;
+					break;
 			}
 		}
 	}
