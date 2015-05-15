0017b3f000e885ba73af5d79a1a12999e747c1c0
Bassons <rogerbassons@gmail.com>
2015-05-15 20:42:04 +0200

    preparat pel doxygen

--- src/DibuixadorAsteroides.java
+++ src/DibuixadorAsteroides.java
@@ -11,36 +11,51 @@ import java.util.Iterator;
 import java.awt.event.KeyListener;
 import java.awt.Font;
 
-// DibuixadorAsteroides dibuixa multiples ObjecteJoc a una finestra(f)
-// f té una mida, un color de fons(c) i un titol determinat. Es pot afegir un KeyListener a f
-// Es poden afegir ObjecteJoc a DibuixadorAsteroides
-// Per poder dibuixar els ObjecteJoc afegits s'ha de crear una finestra
-// La finestra es pot tornar a crear tantes vegades com es desitji, pero nomes s'utilitza la ultima
-// que s'ha creat.
-// La finestra es pot tancar en qualsevol moment
+/// @brief Dibuixa multiples ObjecteJoc a una finestra
+///
+/// La finestra(f) s'ha de crear amb crearFinestra(). f té una mida, un color de fons i un titol determinat.
+///
+/// Es pot afegir un KeyListener a f
+///
+/// Es poden afegir ObjecteJoc a DibuixadorAsteroides. Aquests ObjecteJoc es dibuixaran a f
+///
+/// La finestra es pot tornar a crear tantes vegades com es desitji, pero nomes s'utilitza la ultima
+/// que s'ha creat. La finestra es pot tancar en qualsevol moment.
 
 public class DibuixadorAsteroides {
-	JFrame f_; // finestra principal
-	Canvas c_; // superficie on es dibuixa
-	LinkedList<ObjecteJoc> lo_; // llista d'ObjecteJoc
+	/// @var JFrame f_
+	/// @brief finestra principal
+	
+	/// @var Canvas c_
+	/// @brief superficie on es dibuixa
 
-	int amplada_, altura_; // amplada i altura de c_
+	/// @var LinkedList<ObjecteJoc> lo_
+	/// @brief llista d'ObjecteJoc
 
-	//Pre: --
-	//Post: No hi ha cap finestra
-	//      No hi ha cap KeyListener
-	//      No hi ha cap ObjecteJoc
-	DibuixadorAsteroides()
-	{
+	/// @var int amplada_
+	/// @brief amplada de c_
+
+	/// @var int altura_
+	/// @brief altura de c_
+
+	private JFrame f_;
+	private Canvas c_; 
+	private LinkedList<ObjecteJoc> lo_; 
+	private int amplada_, altura_; 
+
+	/// @pre --
+	/// @post No hi ha cap finestra
+	///       No hi ha cap KeyListener
+	///       No hi ha cap ObjecteJoc
+	DibuixadorAsteroides() {
 		
 		lo_ = new LinkedList<ObjecteJoc>();
 	}
 
-	//Pre: --
-	//Post: crea una finestra per al DibuixaAsteroides amb una superficie amplada x altura, el color de fons es fons
-	//      i el titol és t
-	public void crearFinestra(int amplada, int altura, Color fons, String t)
-	{
+	/// @pre --
+	/// @post crea una finestra per al DibuixaAsteroides amb una superficie amplada x altura, el color de fons es _fons_
+	///      i el titol és t
+	public void crearFinestra(int amplada, int altura, Color fons, String t) {
 		amplada_ = amplada;
 		altura_ = altura;
 		
@@ -64,43 +79,33 @@ public class DibuixadorAsteroides {
 		c_.createBufferStrategy(2);
 	}
 
-	//Pre: oj != null
-	//Post: Afageix oj al DibuixaAsteroides si oj no hi era previament altrament no fa res
-	//      Excepcio: llança excepcio si oj == null
-	public void afegir(ObjecteJoc oj) throws Exception
-	{
-		if (oj == null) {
-			throw new Exception("l'objecte es null");
-		}
-		if (!lo_.contains(oj)) {
+	/// @pre ---
+	/// @post Si oj no era al DibuixadorAsteroides llavors l'afageix altrament no fa res
+	public void afegir(ObjecteJoc oj) {
+		if (oj != null && !lo_.contains(oj)) {
 			lo_.add(oj);
 		}
 	}
 
-	//Pre: oj != null
-	//Post: Elimina oj del DibuixaAsteroides si hi era previament altrament no fa res
-	//      Excepcio: llança excepcio si oj == null
-	public void elimina(ObjecteJoc oj) throws Exception
-	{
-		if (oj == null) {
-			throw new Exception("l'objecte es null");
+	/// @pre ---
+	/// @post Si oj era al DibuixadorAsteroides llavors l'elimina altrament no fa res
+	public void elimina(ObjecteJoc oj) {
+		if (oj != null) {
+			lo_.remove(oj);
 		}
-		lo_.remove(oj);
 	}
 
-	//Pre: l != null
-	//Post: afageix l a la finestra del DibuixaAsteroides
-	public void afegirKeyListener(KeyListener l)
-	{
+	/// @pre ---
+	/// @post afageix l a la finestra del DibuixaAsteroides
+	public void afegirKeyListener(KeyListener l) {
 		f_.addKeyListener(l);
 	}
 
 
-	//Pre: finestra creada
-	//Post: pinta a la superficie de la finestra del DibuixaAsteroides amb el color de fons i pinta tots els ObjecteJoc
-	//      que s'han afegit al DibuixaAsteroides
-	public void dibuixar(int puntuacio)
-	{
+	/// @pre finestra creada
+	/// @post pinta a la superficie de la finestra del DibuixaAsteroides amb el color de fons i pinta tots els ObjecteJoc
+	///       que s'han afegit al DibuixaAsteroides
+	public void dibuixar(int puntuacio) {
 		BufferStrategy buff = c_.getBufferStrategy();
 		Graphics g = buff.getDrawGraphics();
 		g.clearRect(0,0,amplada_,altura_);
@@ -121,11 +126,9 @@ public class DibuixadorAsteroides {
 		g.dispose();
 	}
 
-	//Pre: el DibuixaAsteroides té finestra
-	//Post: s'ha tancat la finestra del DibuixaAsteroides
-	public void tancarFinestra()
-	{
+	/// @pre el DibuixaAsteroides té finestra
+	/// @post s'ha tancat la finestra del DibuixaAsteroides
+	public void tancarFinestra() {
 		f_.dispose();
 	}
-
 }
